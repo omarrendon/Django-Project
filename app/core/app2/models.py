7d2c27dcc0f3e63789fb550760a297c091d68864
Omar <omarcr.96@gmail.com>
2020-09-14 12:42:42 -0500

    Se creo el modelo de la DB

---
+++ app/core/app2/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class ImpresoraDjango(models.Model):
+  id = models.IntegerField(primary_key=True)
+  nombre_impresora = models.TextField(max_length=50)
+  modelo = models.TextField(max_length=50)
+  marca = models.TextField(max_length=50) 
+  fecha_compra = models.DateField('fecha de compra')
+  ultimo_mantenimiento = models.DateField('fecha de ultimo mantenimiento')
+  ubicacion_fisica = models.TextField(max_length=50)
+  nombre_responsable = models.TextField(max_length=50)
+  correo_responsable = models.EmailField()
+  ip = models.TextField(max_length=50)
+  
